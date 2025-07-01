4776f37e29407384c68bc1fe3c577ce6fb62b526
surendranadimpalli <surendranadimpalli45@gmail.com>
2025-07-01 05:59:43 +0530

    Add files via upload

---
+++ area of rectangleand circle program .py
@@ -0,0 +1,15 @@
+# Area of Rectangle
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+print(f"The area of the rectangle is: {area}")
+# Area of Circle
+
+import math  # to use math.pi
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = math.pi * radius ** 2
+print(f"The area of the circle is: {area}")
